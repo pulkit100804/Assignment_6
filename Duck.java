fd6c9d187521390f95c906a7ca02f3d34ded4e98
Pulkit Shrivastava <167713133+pulkit100804@users.noreply.github.com>
2025-03-08 22:55:20 +0530

    Create Duck.java

---
+++ Duck.java
@@ -0,0 +1,22 @@
+abstract class Duck {
+    QuackBehaviour quackBehaviour;
+    SwimBehaviour swimBehaviour;
+    FlyBehaviour flyBehaviour;
+
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
+        this.swimBehaviour = swimBehaviour;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+
+    abstract public void display();
+
+    public void performQuack() {
+        quackBehaviour.quack();
+    }
